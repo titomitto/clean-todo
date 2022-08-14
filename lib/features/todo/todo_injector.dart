bb079a23e8d742be8627499716c568af39516fb2
Tito Mitto <mordeccai9@gmail.com>
2022-08-14 20:26:07 +0300

    todos

--- lib/features/todo/todo_injector.dart
+++ lib/features/todo/todo_injector.dart
@@ -1,12 +1,12 @@
 import 'package:clean_todo/core/injector.dart';
 import 'package:clean_todo/core/presentation/screens/screen.dart';
-import 'package:clean_todo/features/todo/domain/usecases/add_count.dart';
-import 'package:clean_todo/features/todo/presentation/view_models/add_todo_view_model.dart';
+import 'package:clean_todo/features/todo/domain/usecases/add_todo.dart';
+import 'package:clean_todo/features/todo/presentation/viewmodels/add_todo_view_model.dart';
+import 'package:clean_todo/features/todo/presentation/viewmodels/todo_list_view_model.dart';
 import 'package:clean_todo/features/todo/presentation/widgets/todos_view_model.dart';
 import 'package:get_it/get_it.dart';
-import 'domain/usecases/increment_count.dart';
+import 'domain/usecases/delete_todo.dart';
 import 'presentation/screens/todos_screen.dart';
-import 'presentation/view_models/todo_list_view_model.dart';
 
 class TodoInjector extends Injector {
   @override
