1a07852fa76ce9981b6ff2a5ed217fc87af5ad27
Tito Mitto <mordeccai9@gmail.com>
2023-03-22 11:12:26 +0300

    fix: return add task

--- lib/features/tasks/presentation/screens/tasks_screen.dart
+++ lib/features/tasks/presentation/screens/tasks_screen.dart
@@ -1,4 +1,3 @@
-import 'package:clean_todo/features/preferences/presentation/screens/preferences_screen.dart';
 import 'package:clean_todo/features/tasks/presentation/widgets/tasks_list_view.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
