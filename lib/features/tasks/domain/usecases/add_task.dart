d70cd9cdfc3a030fa2a339d7819a2994eed3be00
Tito Mitto <mordeccai9@gmail.com>
2023-04-14 10:56:48 +0300

    feat: add validation to usecase

--- lib/features/tasks/domain/usecases/add_task.dart
+++ lib/features/tasks/domain/usecases/add_task.dart
@@ -6,23 +6,7 @@ import 'package:clean_todo/features/tasks/domain/repositories/task_repository.da
 import 'package:dartz/dartz.dart' hide Task;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final addTaskUseCaseProvider = Provider<AddTask>((ref) {
-  final repository = ref.read(tasksRepositoryProvider);
-  return AddTask(repository: repository);
-});
-
-class AddTask extends UseCase<Future<Either<Failure, void>>, AddTaskParams> {
-  TasksRepository repository;
-
-  AddTask({
-    required this.repository,
-  });
-
-  @override
-  Future<Either<Failure, void>> call(AddTaskParams params) {
-    return repository.addTask(params.task);
-  }
-}
+import '../validators/task_validator.dart';
 
 class AddTaskParams {
   Task task;
@@ -30,3 +14,33 @@ class AddTaskParams {
     required this.task,
   });
 }
+
+class AddTask extends UseCase<Future<Either<Failure, void>>, AddTaskParams> {
+  TasksRepository repository;
+  TaskValidator validator;
+
+  AddTask({
+    required this.repository,
+    required this.validator,
+  });
+
+  @override
+  Future<Either<Failure, void>> call(AddTaskParams params) async {
+    ValidationFailure? validationFailure = validator.validate(params.task);
+
+    if (validationFailure != null) {
+      return Left(validationFailure);
+    }
+
+    return repository.addTask(params.task);
+  }
+}
+
+final addTaskUseCaseProvider = Provider<AddTask>((ref) {
+  final validator = TaskValidator();
+  final repository = ref.read(tasksRepositoryProvider);
+  return AddTask(
+    repository: repository,
+    validator: validator,
+  );
+});
