7e91d31c063ceb7f90bf476f0ee557b208547a76
Tito Mitto <mordeccai9@gmail.com>
2023-04-18 12:52:21 +0300

    refactor: remove unused repository

--- lib/features/tasks/data/datasources/tasks_remote_datasource.dart
+++
@@ -1,9 +0,0 @@
-import 'package:clean_todo/core/core.dart';
-import '../models/task_model.dart';
-
-abstract class RemoteLocalDataSource implements LocalDataSource {
-  Future<List<TaskModel>> getTasks();
-  Future<void> addTask(String title);
-  Future<void> updateTask(TaskModel task);
-  Future<void> deleteTask(TaskModel task);
-}
